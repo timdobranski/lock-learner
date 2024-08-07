18d8fcf48cf9fe51216a9bee767630c6f730b20d
Tim <timdobranski@gmail.com>
2024-08-07 14:31:58 -0700

    Built out lobby page username management

--- app/lobby/page.js
+++ app/lobby/page.js
@@ -1,13 +1,66 @@
+'use client';
+
+import styles from './page.module.css';
+import React, { useState, useEffect } from 'react';
 
 
 export default function Lobby() {
+  const [username, setUsername] = useState('');
+  const [savedUsername, setSavedUsername] = useState('');
+  const [matchLink, setMatchLink] = useState('');
+
+
+  useEffect(() => {
+    // Retrieve username from localStorage if it exists
+    const storedUsername = localStorage.getItem('username');
+    if (storedUsername) {
+      setSavedUsername(storedUsername);
+    }
+  }, []);
+
+  const handleChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem('username', username);
+    setSavedUsername(username);
+  }
+
+  const resetUsername = () => {
+    localStorage.removeItem('username');
+    setSavedUsername('');
+  }
 
   return (
-    <div className='infoCard'>
-      <h1>LOBBY</h1>
-      <p>Here you can generate a match link and send it to another player.</p>
-      <p>When they click the match link, a countdown will begin to start the match.</p>
-      <p>{`At the end of the countdown, you'll both be given the same combo, and you can race to see who opens their lock first. `}</p>
+    <div className={styles.lobbyContainer}>
+      <div className='infoCard'>
+        <h1>LOBBY</h1>
+
+        { savedUsername ?
+          <div className={styles.usernameWrapper}>
+            <p>Playing As:</p>
+            <p className={styles.username}>{savedUsername}</p>
+            <button onClick={resetUsername} className='redButton'>CHANGE USERNAME</button>
+          </div>
+          :
+          <form onSubmit={handleSubmit} className={styles.usernameInputWrapper}>
+            <label className={styles.usernameLabel} htmlFor='usernameInput'>Choose A Username:</label>
+            <input type="text" id='usernameInput' value={username} onChange={handleChange} className={styles.usernameInput}/>
+            <button type="submit" className='redButton'>SAVE</button>
+          </form>}
+
+
+        <p>Here you can generate a match link and send it to another player.</p>
+        <p>When they click the match link, a countdown will begin to start the match.</p>
+        <p>{`At the end of the countdown, you'll both be given the same combo, and you can race to see who opens their lock first. `}</p>
+
+
+      </div>
+
+
+
     </div>
   )
 }
--- components/Lock/Lock.jsx
+++ components/Lock/Lock.jsx
@@ -26,7 +26,7 @@ export default function Lock({ combo, setCombo, step, setStep, currentLockNum, s
   const handleMouseDown = (event) => {
     if (event.type === 'mousedown') {
       event.preventDefault();
-  }
+    }
     const rect = lockFaceRef.current.getBoundingClientRect();
     const centerX = rect.left + rect.width / 2 + offsetX;
     const centerY = rect.top + rect.height / 2 + offsetY;
@@ -52,14 +52,14 @@ export default function Lock({ combo, setCombo, step, setStep, currentLockNum, s
   const handleMouseMove = (event) => {
     if (event.type === 'mousemove') {
       event.preventDefault();
-  }
+    }
 
     if (lastPosition) {
       const initialAngle = computeAngle({ clientX: lastPosition.x, clientY: lastPosition.y });
       const currentAngle = computeAngle(event);
       const angleDelta = currentAngle - initialAngle;
       setRotation(prev => (prev + angleDelta + 360) % 360);
-            setLastPosition({ x: event.clientX, y: event.clientY });
+      setLastPosition({ x: event.clientX, y: event.clientY });
     }
   };
   const handleTouchMove = () => {
@@ -72,42 +72,42 @@ export default function Lock({ combo, setCombo, step, setStep, currentLockNum, s
     setLastPosition(null);
   };
   const handleTouchEnd = () => {
-  handleMouseUp(); // Reuse the mouse up logic
+    handleMouseUp(); // Reuse the mouse up logic
   };
   useEffect (() => {
     console.log('step: ', step);
     // check if the combo is valid whenever we move to step 1 (directions)
     if (step === 1) {
-        // Check for adjacent duplicates, ignoring empty strings
-    const hasAdjacentDuplicates = combo.some((value, index, arr) => {
+      // Check for adjacent duplicates, ignoring empty strings
+      const hasAdjacentDuplicates = combo.some((value, index, arr) => {
 
-      if (index > 0 && value === arr[index - 1] && value !== '') {
-        return true;
+        if (index > 0 && value === arr[index - 1] && value !== '') {
+          return true;
+        }
+        return false;
+      });
+      if (hasAdjacentDuplicates) {
+        console.log('Invalid combo, setting step back to 0');
+        alert('This is an invalid combo. No combo will ever have two adjacent numbers the same. Please choose another combo.');
+        // Set combo to some default value if needed
+        setCombo(['', '', '']);
       }
-      return false;
-    });
-    if (hasAdjacentDuplicates) {
-      console.log('Invalid combo, setting step back to 0');
-      alert('This is an invalid combo. No combo will ever have two adjacent numbers the same. Please choose another combo.');
-      // Set combo to some default value if needed
-      setCombo(['', '', '']);
-    }
-    const hasMissingDigits = combo.some((value, index, arr) => {
+      const hasMissingDigits = combo.some((value, index, arr) => {
 
-      if (value === '') {
-        return true;
+        if (value === '') {
+          return true;
+        }
+        return false;
+      });
+      if (hasMissingDigits) {
+        console.log('Invalid combo, setting step back to 0');
+        alert('This is an invalid combo. Please fill in all the numbers.');
+        // Set combo to some default value if needed
+        setCombo(['', '', '']);
       }
-      return false;
-    });
-    if (hasMissingDigits) {
-      console.log('Invalid combo, setting step back to 0');
-      alert('This is an invalid combo. Please fill in all the numbers.');
-      // Set combo to some default value if needed
-      setCombo(['', '', '']);
-    }
-    // combo is valid, move to next step
-    setRotation(0);
-    setLastPosition(null);
+      // combo is valid, move to next step
+      setRotation(0);
+      setLastPosition(null);
     }
     // if (step === 2) {
     //   setCurrentNum(0);
@@ -133,25 +133,27 @@ export default function Lock({ combo, setCombo, step, setStep, currentLockNum, s
 
     // Cleanup
     return () => {
-        lockFaceElem.removeEventListener('touchstart', touchStartListener);
-        lockFaceElem.removeEventListener('touchmove', touchMoveListener);
+      lockFaceElem.removeEventListener('touchstart', touchStartListener);
+      lockFaceElem.removeEventListener('touchmove', touchMoveListener);
     };
-}, []);
-useEffect(() => {
+  }, []);
+  useEffect(() => {
   // Normalize the rotation to be between 0 and 360
-  const effectiveRotation = (rotation - 4.5) % 360;
+    const effectiveRotation = (rotation - 4.5) % 360;
 
-  // If the effectiveRotation is negative (due to the modulo operation), add 360 to normalize
-  const normalizedRotation = effectiveRotation < 0 ? effectiveRotation + 360 : effectiveRotation;
+    // If the effectiveRotation is negative (due to the modulo operation), add 360 to normalize
+    const normalizedRotation = effectiveRotation < 0 ? effectiveRotation + 360 : effectiveRotation;
 
-  const reversedRotation = 360 - normalizedRotation;
-  const newSectionNumber = Math.floor(reversedRotation / 9) % 40; // Using % 40 to ensure it wraps around to 0 when it hits 40
-  setCurrentLockNum(newSectionNumber);
-}, [rotation]);
+    const reversedRotation = 360 - normalizedRotation;
+    const newSectionNumber = Math.floor(reversedRotation / 9) % 40; // Using % 40 to ensure it wraps around to 0 when it hits 40
+    setCurrentLockNum(newSectionNumber);
+  }, [rotation]);
 
 
   return (
     <>
+      {/* {(step === 2 || step === 3 || step === 4) && <div className={`${styles.swipableAreaIndicator} ${styles.lock}`}></div>} */}
+
       {/* lock top bar, open */}
       <div className={`${styles.lock} ${step < 2 ? styles.hidden : ''} ${step === 5 ? styles.lockBarOpen : ''}`}>
         <Image src={lockBar} height={600} alt="Lock" />
@@ -181,8 +183,6 @@ useEffect(() => {
         style={{ transform: `rotate(${rotation}deg)` }}
       >
         <Image src={lockFace} height={600} alt="Lock" className={styles.lockFace} />
-        <div className={styles.swipableAreaIndicator}>
-        </div>
       </div>
       {/* current combo number indicator */}
       {step < 5 && (
