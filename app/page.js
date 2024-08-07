4b003de7c74a71874ee14fdfdbce5fda14b8b5ed
Tim <timdobranski@gmail.com>
2024-08-06 21:06:15 -0700

    Added info card to home page

--- app/page.js
+++ app/page.js
@@ -14,6 +14,11 @@ export default function Home() {
 
   return (
     <div className={styles.pageWrapper}>
+      <div className='infoCard'>
+        <p>Welcome to Lock Learner. Here you can practice opening your combination lock.</p>
+        <p>Coming soon: challenge friends to a lock race to test your skills!</p>
+      </div>
+
       <button className={styles.navButton} onClick={() => router.push('/practice')}>PRACTICE</button>
       <button className={styles.navButton} onClick={() => alert('Challenge mode coming soon. Invite a friend and race them with a random combination!')}>CHALLENGE</button>
       {/* <FontAwesomeIcon icon={faGear} className={styles.settingsIcon} onClick={() => router.push('/settings')} /> */}
